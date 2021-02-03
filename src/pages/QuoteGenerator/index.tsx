822c685004dbf5da7b71fdae5efe78b9e06976ac
SUTRIXMEDIA1\huy.ta <huy.ta@sutrixsolutions.com>
2021-02-03 18:14:28 +0700

    Feat: implement function call api quotes, randomColor

--- src/pages/QuoteGenerator/index.tsx
+++ src/pages/QuoteGenerator/index.tsx
@@ -1,73 +1,51 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 import QuoteItem from '../../components/Quote/item';
 import './style.scss';
 import { getAllQuotes } from '../../services/quotes';
 
 const QuoteGenerator: React.FC = () => {
-  const colors = [
-    '#16a085',
-    '#27ae60',
-    '#2c3e50',
-    '#f39c12',
-    '#e74c3c',
-    '#9b59b6',
-    '#FB6964',
-    '#342224',
-    '#472E32',
-    '#BDBB99',
-    '#77B1A9',
-    '#73A857',
-  ];
+  const [quotesList, setQuotesList] = useState<object[]>([]);
 
-  const [quotesList, setQuotesList] = useState([]);
+  const [color, setColor] = useState<string>('');
 
-  const [color, setColor] = useState('');
+  const [text, setText] = useState<string>('');
 
-  const [text, setText] = useState('');
-
-  const [author, setAuthor] = useState('');
-
-  const quoteItemRef = useRef(null);
+  const [author, setAuthor] = useState<string>('');
 
   useEffect(() => {
-    // console.log('quotesList', quotesList);
-    const handleCallApi = async () => {
-      const res: any = await getAllQuotes();
-      console.log('res', res);
-      setQuotesList(res);
-      console.log('quotesList', quotesList);
-    };
     handleCallApi();
   }, []);
 
   useEffect(() => {
     handleRandomQuote();
-  }, []);
+  }, [quotesList]);
 
-  const getRandomColor = (): number => {
-    return ~~(Math.random() * colors.length);
+  const getRandomColor = (): string => {
+    const LETTERS = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += LETTERS[~~(Math.random() * LETTERS.length)];
+    }
+    return color;
   };
 
   const getRandomQuote = (): number => {
     return ~~(Math.random() * quotesList.length);
   };
 
+  const handleCallApi = async () => {
+    const res: any = await getAllQuotes();
+    setQuotesList(res);
+  };
+
   const handleRandomQuote = (): void => {
-    const indexColor: number = getRandomColor();
+    const newColor: string = getRandomColor();
     const indexQuote: number = getRandomQuote();
 
-    console.log('color index: ', indexColor);
-    console.log('quote index: ', indexQuote);
-
     const quoteObj: any = quotesList[indexQuote];
-    const newColor: string = colors[indexColor];
 
-    console.log('quoteObj', quoteObj);
-
-    if (quoteObj) {
-      setText(quoteObj?.quote);
-      setAuthor(quoteObj?.author);
-    }
+    setText(quoteObj?.quote);
+    setAuthor(quoteObj?.author);
     setColor(newColor);
   };
 
